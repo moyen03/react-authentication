219c2c3e2dbec9308dc7515b50883f01993d4125
Moyen Uddin <moyen.ruet@yahoo.com>
2024-05-29 07:10:07 +0200

    hh

--- front-end/src/auth/PrivateRoute.js
+++ front-end/src/auth/PrivateRoute.js
@@ -1,10 +1,10 @@
-import { Redirect, Route } from "react-router-dom";
-import { useUser } from "./useUser";
+import { Redirect, Route } from 'react-router-dom';
+import { useUser } from './useUser';
 
-export const PrivateRoute = (props) => {
-  const user = useUser();
+export const PrivateRoute = props => {
+    const user = useUser();
 
-  if (!user) return <Redirect to="/login" />;
+    if (!user) return <Redirect to="/login" />
 
-  return <Route {...props} />;
-};
+    return <Route {...props} />
+}
